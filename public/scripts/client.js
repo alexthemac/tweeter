03844306dc01da988fe8bb4ee6528b6b3f6dc865
Alex MacLure <almaclure@gmail.com>
2021-08-13 03:03:21 +0000

    Screenshots added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,7 @@
 $(document).ready(function() {
 
   //Takes tweet data and formats it into HTML
-  const createTweetElement = function (tweetObj) {
+  const createTweetElement = function(tweetObj) {
 
     const $tweet = `<article class="tweet-article">
                       <header>
@@ -26,11 +26,11 @@ $(document).ready(function() {
                           <div class="heart"><i class="fas fa-heart"></i></div>
                         </div>
                       </footer>
-                    </article>`
+                    </article>`;
     return $tweet;
-  }
+  };
 
-  //Renders tweets
+  //Renders tweets on DOM
   const renderTweets = function(tweets) {
 
     let $returnTweetHTML;
@@ -39,37 +39,36 @@ $(document).ready(function() {
     for (const tweet of tweets) {
       //passes each tweet data to createTweetElement function
       $returnTweetHTML = createTweetElement(tweet);
-      //returned HTML is then prepended to index.html (added at top: prepend. added at bottom: append)
-      $('.tweets-container').prepend($returnTweetHTML)
+      //returned HTML is then prepended to index.html
+      $('.tweets-container').prepend($returnTweetHTML);
     }
-  }
+  };
 
   //Sends tweets located at /tweets to renderTweets function
   const loadTweets = function() {
     //Performs GET request
     $.ajax({url: '/tweets', method: 'GET', dataType: 'JSON'})
     //Once data recieved (async), send to renderTweets function
-    .then(function(response) {
-      renderTweets(response)
-    });
-  }
+      .then(function(response) {
+        renderTweets(response);
+      });
+  };
 
-  //Calls loadTweets on page load. This allows it to run asynchronously. Once ready the response is sent to the renderTweets function.
   loadTweets();
 
   //Sends last entered tweet to renderTweets function
-  const loadLastTweet = function () {
+  const loadLastTweet = function() {
     $.ajax({url: '/tweets', method: 'GET', dataType: 'JSON'})
     //Once data recieved (async), send to renderTweets function
-    .then(function(response) {
-      //Last item in response array is last tweet 
-      const lastTweet = response[response.length - 1];
-      //passes the last created tweet data (last element in response array) to createTweetElement function
-      $returnTweetHTML = createTweetElement(lastTweet);
-      //returned HTML is then prepended to index.html (added at top: prepend. added at bottom: append)
-      $('.tweets-container').prepend($returnTweetHTML);
-    });
-  }
+      .then(function(response) {
+      //Last item in response array is last tweet
+        const lastTweet = response[response.length - 1];
+        //passes the last created tweet data (last element in response array) to createTweetElement function
+        $returnTweetHTML = createTweetElement(lastTweet);
+        //returned HTML is then prepended to index.html (added at top: prepend. added at bottom: append)
+        $('.tweets-container').prepend($returnTweetHTML);
+      });
+  };
 
   //Listens for the submit event on tweet button. When pressed, sends entered tweet to the tweetData "Database"
   $("#tweet-entry-area").on("submit", function(event) {
@@ -81,31 +80,31 @@ $(document).ready(function() {
     //Actual text typed in
     const $typedTweet = $typedTweetObj.val();
 
-    //Alert if tweet entered is empty or too long. 
+    //Alert if tweet entered is empty or too long.
     if (!$typedTweet) {
       //Adds error message at top of new-tweet section
       $(".error").remove();
-      $('.new-tweet').prepend(`<div class="error"><i class="fas fa-exclamation-triangle"></i>&nbsp&nbsp&nbspTweet is empty. Please try again&nbsp&nbsp&nbsp<i class="fas fa-exclamation-triangle"></i></div>`)
+      $('.new-tweet').prepend(`<div class="error"><i class="fas fa-exclamation-triangle"></i>&nbsp&nbsp&nbspTweet is empty. Please try again&nbsp&nbsp&nbsp<i class="fas fa-exclamation-triangle"></i></div>`);
     } else if ($typedTweet.length > 140) {
       //Adds error message at top of new-tweet section
       $(".error").remove();
-      $('.new-tweet').prepend(`<div class="error"><i class="fas fa-exclamation-triangle"></i>&nbsp&nbsp&nbspTweet is too long (140 characters max)! Please try again&nbsp&nbsp&nbsp<i class="fas fa-exclamation-triangle"></i></div>`)
+      $('.new-tweet').prepend(`<div class="error"><i class="fas fa-exclamation-triangle"></i>&nbsp&nbsp&nbspTweet is too long (140 characters max)! Please try again&nbsp&nbsp&nbsp<i class="fas fa-exclamation-triangle"></i></div>`);
     //If tweet is correct size and not empty, send tweet data to database.
     } else {
-      //Removes error message, if there is one. 
+      //Removes error message, if there is one.
       $(".error").remove();
 
       //String represented typed in tweet but in standard URL-encoded notation
-      const $serializedTweet = $typedTweetObj.serialize() 
+      const $serializedTweet = $typedTweetObj.serialize();
 
       //Send entered tweet data to the tweetData "database"
       $.ajax({url: '/tweets', data: $serializedTweet, method: 'POST'})
-      .then(function(response) {
+        .then(function(response) {
         //Loads the most recently created tweet on top of other tweets
-        loadLastTweet();
-      })
-    };
-  })
+          loadLastTweet();
+        });
+    }
+  });
 
   //Transform text entered to prevent an XSS attack with escaping (is used inside createTweetElement function)
   const escape = function(str) {
@@ -114,8 +113,11 @@ $(document).ready(function() {
     return div.innerHTML;
   };
 
+  //Hide new tweet entry area on start
+  $(".new-tweet").hide();
+
   //Toggle new tweet entry area when "Write a new tweet" is clicked
-  $(".WriteANewTweet").click(function(){
+  $(".WriteANewTweet").click(function() {
     $(".new-tweet").toggle();
   });
 });
